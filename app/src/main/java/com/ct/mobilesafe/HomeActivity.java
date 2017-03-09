4fc60b1dd85dc3fdf498ed2db52894c69681b2a2
YooTrepeat <crystal.t.yoo@icloud.com>
2017-03-09 21:30:01 +0800

    second commit

---
+++ app/src/main/java/com/ct/mobilesafe/HomeActivity.java
@@ -0,0 +1,31 @@
+package com.ct.mobilesafe;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by Crystal on 2017/3/9.
+ */
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initView();
+        initData();
+        setListener();
+    }
+
+    private void initView() {
+
+    }
+
+    private void initData() {
+    }
+
+    private void setListener() {
+    }
+}
--- app/src/main/java/com/ct/mobilesafe/SplashActivity.java
+++ app/src/main/java/com/ct/mobilesafe/SplashActivity.java
@@ -1,5 +1,8 @@
 package com.ct.mobilesafe;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -7,15 +10,27 @@ import android.widget.TextView;
 public class SplashActivity extends AppCompatActivity {
 
     private TextView tvVersion;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        context = this;
+
         initView();
         initData();
         setListener();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SystemClock.sleep(2000);
+
+                startActivity(new Intent(context, HomeActivity.class));
+            }
+        }).start();
     }
 
     private void initView() {
